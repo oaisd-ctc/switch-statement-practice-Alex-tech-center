840ba25c2fa52343628303a09a0efec002831c01
Alex <aljo205@student.oaisd.org>
2024-10-21 18:06:26 +0000

    switch statement built

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System; 
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+
+            Console.WriteLine("Choose a number ranging from 0 to 10.");
+            int num = int.Parse (Console.ReadLine());
+
+            switch(num) {
+
+                case 0:
+                    Console.WriteLine("Your number is zero.");
+                    break;
+
+                case 1:
+                    Console.WriteLine("Your number is one.");
+                    break;
+
+                case 2: 
+                    Console.WriteLine("Your number is two.");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Your number is three.");
+                    break;
+                
+                case 4: 
+                    Console.WriteLine("Your number is four.");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Your number is five.");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Your number is six.");
+                    break;
+                case 7: 
+                    Console.WriteLine("Your number is seven.");
+                    break;
+
+                case 8:
+                    Console.WriteLine("Your number is seven.");
+                    break;
+
+                case 9:
+                    Console.WriteLine("Your number is nine.");
+                    break;
+                case 10:
+                    Console.WriteLine("Your number is ten.");
+                    break;
+
+            }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
